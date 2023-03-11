b3217d13d72065d52566ce4135944363dda412d1
nowm1006 <nowm1006@yahoo.co.jp>
2023-03-11 10:02:23 +0900

    added header added font awesome

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+</script>
+
+<header>
+	<div class="flex h-8 items-center justify-center bg-gray-700">
+		<i class="fa-solid fa-list-check" />
+		<span class="font-bold text-white">TaskChute</span>
+		<span class="font-light text-white">Clone</span>
+	</div>
+</header>
+<slot />
